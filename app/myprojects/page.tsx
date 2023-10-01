d50c38bee2347385baa513a5f036326d514a1f24
Billy <billy.iv.art@gmail.com>
2023-09-30 20:19:11 -0600

    Created page folders for portfolios other pages.

---
+++ app/myprojects/page.tsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>PROJ, Next.js!</h1>
+      <p>This is a simple Next.js page.</p>
+    </div>
+  );
+}
+
+

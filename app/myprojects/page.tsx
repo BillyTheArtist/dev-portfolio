bd3ff0e77798f628ab3a249b7e79d7457d100080
BillyTheArtist <billy.iv.art@gmail.com>
2023-10-02 21:32:16 -0600

    Changed the way projects are displaying with the ResponsiveWindow by mapping through project objects>

--- app/myprojects/page.tsx
+++ app/myprojects/page.tsx
@@ -1,17 +1,31 @@
 import { ResponsiveWindow } from '@/components/ResponsiveWindow'
-
+import { Montserrat } from 'next/font/google'
 import React from 'react'
+import { projects } from './data'
+
+const montserrat = Montserrat({
+  weight: '400',
+  subsets: ['latin']
+})
+const montserratSemiBold = Montserrat({
+  weight: '500',
+  subsets: ['latin']
+})
+const montserratBold = Montserrat({
+  weight: '600',
+  subsets: ['latin']
+})
 
 export default function HomePage () {
   return (
-    <div>
-      <ResponsiveWindow
-        title='Project'
-        image='/images/shapeSVG.svg'
-        description=''
-        link=''
-        gradient=''
-      />
-    </div>
+    <main className={montserratBold.className}>
+      <div className='flex flex-col min-h-screen bg-altBG'>
+        <div className='mt-20 m-2 md:mx-60 lg:mx-60'>
+          {projects.map((project, index) => (
+            <ResponsiveWindow projectProp={project} key={index} />
+          ))}
+        </div>
+      </div>
+    </main>
   )
 }

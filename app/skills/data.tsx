17a80d62e419eca6edabf7d4aa47dff724eeea26
BillyTheArtist <billy.iv.art@gmail.com>
2023-10-03 15:24:34 -0600

    Improved the way I am rendering my skills CodeCard component.

---
+++ app/skills/data.tsx
@@ -0,0 +1,10 @@
+import { Skill } from "@/types/skill"
+import { TSIcon } from "@/components/icons/TSIcon"
+
+export const skills: Skill[] = [
+    {
+icon: <TSIcon className='mr-3' />,
+skill: 'TypeScript',
+description: 'Jimble jamble fimble famble Brimble Bramble Gimble Gamble Jimble jamble fimble szxrfesxfzr Brimble Bramble secrcergs Gamble Jimble saexfrxefras fimble famble Brimble Bramble Gimble Gamble Jimble jasertgccsrgemble fimble famble Brimsergvsevrtgble Bramble Gimble Gamble Jimble jamble fimble famble scdrgcrgs Brevsrtsgvteramble Gimble Gamble Jimble jamble srxsxrfg famble Brimble Bramble Gimble Gamble Jimble jamble fimble famble Brimble Bramble Gimble Gamble'
+    },
+]
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -4,6 +4,7 @@ import { JSIcon } from "@/components/icons/JSIcon";
 import { TSIcon } from "@/components/icons/TSIcon";
 import { NextIcon } from "@/components/icons/NextIcon";
 import { CodeCard } from "@/components/CodeCard";
+import { skills } from "./data";
 
 const montserratLight = Montserrat({
   weight: "300",
@@ -33,76 +34,9 @@ export default function HomePage() {
         <div
           className={` text-sm text-brightGrey lg:p-10 lg:m-40 bg-gradient-to-b from-hiBG to-blueGrey shadow-around grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 leading-relaxed ${montserratSemiBold.className}`}
         >
-          <CodeCard
-            title="JavaScript"
-            icon={<JSIcon className={`mr-3`} />}
-            description="Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I'm committed to transforming complex concepts
-            into user-friendly solutions. Let's collaborate on your next
-            project and bring your vision to life."
-          />
-          <CodeCard
-            title="TypeScript"
-            icon={<TSIcon className={`mr-3`} />}
-            description="Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I'm committed to transforming complex concepts
-            into user-friendly solutions. Let's collaborate on your next
-            project and bring your vision to life."
-          />
-
-          <CodeCard
-            title="Nextjs"
-            icon={<NextIcon className={`mr-3`} />}
-            description="Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I'm committed to transforming complex concepts
-            into user-friendly solutions. Let's collaborate on your next
-            project and bring your vision to life."
-          />
-          <div className={` m-3 p-4 border-2 border-almostBlack rounded-md `}>
-            Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I&apos;m committed to transforming complex concepts
-            into user-friendly solutions. Let&apos;s collaborate on your next
-            project and bring your vision to life.
-          </div>
-          <div className={` m-3 p-4 border-2 border-almostBlack rounded-md `}>
-            Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I&apos;m committed to transforming complex concepts
-            into user-friendly solutions. Let&apos;s collaborate on your next
-            project and bring your vision to life.
-          </div>
-          <div className={` m-3 p-4 border-2 border-almostBlack rounded-md `}>
-            Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I&apos;m committed to transforming complex concepts
-            into user-friendly solutions. Let&apos;s collaborate on your next
-            project and bring your vision to life.
-          </div>
-          <div className={` m-3 p-4 border-2 border-almostBlack rounded-md `}>
-            Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I&apos;m committed to transforming complex concepts
-            into user-friendly solutions. Let&apos;s collaborate on your next
-            project and bring your vision to life.
-          </div>
-          <div className={` m-3 p-4 border-2 border-almostBlack rounded-md `}>
-            Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I&apos;m committed to transforming complex concepts
-            into user-friendly solutions. Let&apos;s collaborate on your next
-            project and bring your vision to life.
-          </div>
-          <div className={` m-3 p-4 border-2 border-almostBlack rounded-md `}>
-            Blockchain web apps, as well as dynamic marketing sites. With an eye
-            for innovation and a passion for staying on the cutting edge of web
-            development, I&apos;m committed to transforming complex concepts
-            into user-friendly solutions. Let&apos;s collaborate on your next
-            project and bring your vision to life.
-          </div>
+          {skills.map((skill, index) => (
+            <CodeCard skillProp={skill} key={index} />
+          ))}
         </div>
       </div>
     </main>

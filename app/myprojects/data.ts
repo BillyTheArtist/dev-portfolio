d8f6ef0b2bb38d550319f4c064bfbd3dcebefde5
BillyTheArtist <billy.iv.art@gmail.com>
2023-10-03 15:22:49 -0600

    Added new key for title color.

--- app/myprojects/data.ts
+++ app/myprojects/data.ts
@@ -1,8 +1,10 @@
 import { Project } from "@/types/project"
 
+
 export const projects: Project[] = [
     {
         title: 'dApp Crypto Platform',
+        titleColor: 'text-brightGrey',
         image: '/images/dappStacker.png',
         description: 'Decentralized application for the DEBTBox coin ecosystem. Users could connect any crypto wallet via Rainbowkit and buy/sell any coin in the DEBT ecosystem. All data was fetched using Wagmi, excecuting smart contracts on the BNB chain to facilitate trustless transactions and automate processes. The DApp was created by a front end team led by myself in close collaboration with a professional web designers.',
         gradient: '/images/lightGradient.svg',
@@ -12,11 +14,33 @@ export const projects: Project[] = [
     },
     {
         title: 'Crypto Web Application',
+        titleColor: 'text-brightGrey',
         image: '/images/webStacker.png',
         description: 'it web',
-        gradient: '/images/purpleGradient.svg',
+        gradient: '/images/siteGradient.svg',
         descriptionGradient: '/images/descriptionGradient.svg',
-        link: 'https://demo-preview.thedebtbox.com/',
+        link: 'https://app.thedebtbox.com/login',
+        glow: 'hover:shadow-goldGlow',
+    },
+    {
+        title: 'Ecosystem Website',
+        titleColor: 'text-brightGrey',
+        image: '/images/ecoStack.png',
+        description: 'it web',
+        gradient: '/images/webGradient.svg',
+        descriptionGradient: '/images/descriptionGradient.svg',
+        link: 'https://www.thedebtbox.com/',
         glow: 'hover:shadow-purpleGlow',
     },
+    {
+        title: 'Crypto Dashboard',
+        titleColor: 'text-nonBlackGrey',
+        image: '/images/fairStack.png',
+        description: 'it web',
+        gradient: '/images/fairGradient.svg',
+        descriptionGradient: '/images/descriptionGradient.svg',
+        link: 'https://www.thefairproject.ai/',
+        glow: 'hover:shadow-fairGlow',
+    },
 ]
+

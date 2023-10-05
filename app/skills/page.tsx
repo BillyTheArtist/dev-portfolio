5cdfb26d7b13652a832ac54f113a9104ca31c8b9
BillyTheArtist <billy.iv.art@gmail.com>
2023-10-04 19:28:25 -0600

    Small visual changes.

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -5,9 +5,9 @@ import { skills } from './data'
 export default function HomePage () {
   return (
     <main>
-      <div className='flex flex-col min-h-screen'>
+      <div className='flex flex-col min-h-[calc(100vh-177px)] justify-center'>
         <div
-          className={`m-4 text-sm text-brightGrey lg:p-10 lg:mt-20 lg:mx-40 bg-gradient-to-b from-hiBG to-blueGrey shadow-around grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 leading-relaxed $font-semibold`}
+          className={`m-2 text-sm text-brightGrey lg:p-10 lg:mx-40 bg-gradient-to-b from-hiBG to-blueGrey shadow-around grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 leading-relaxed $font-semibold`}
         >
           {skills.map((skill, index) => (
             <CodeCard skillProp={skill} key={index} />
--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -7,7 +7,7 @@ import { SeparatorFooter } from '@/components/ui/SeparatorFooter'
 export function PageFooter () {
   return (
     <footer className='h-[177px]'>
-      <span className='flex w-full bg-gradient-to-b from-transparent from 30% via-liteBG via-10% to-niceOrange to-90%  mt-[-15px] md:mt-[-80px] lg:mt-[-10px] pt-[100px] mb-[-50px]' />
+      <span className='flex w-full bg-gradient-to-b from-transparent from 30% via-liteBG via-10% to-niceOrange to-90%  mt-[-15px] md:mt-[-80px] lg:mt-[-10px] pt-[100px] mb-[-50px] lg:mb-[-60px]' />
       <FooterPeaks />
       <div className='grid grid-cols-1 place-items-center bg-boxLite md:space-x-7'>
         <SeparatorFooter />
